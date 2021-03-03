64c0b355d2bcd5ee7341bad8e12df8ead7e19f94
Wes Howard <BigRonald42@hotmail.com>
2021-03-02 20:36:26 -0700

    added html structure, deleted unused CSS

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,78 @@
-var $startButton = document.querySelector("#generate");
+var $startButton = document.querySelector("#startbutton");
 var $quizWindow = document.querySelector("#window");
+var $timerElement = document.querySelector("#timer");
 var $highScore = document.querySelector("#highscore");
 var $scoreBoard = document.querySelector("#scoreboard");
 
+var timeLeft;
 
-//this is an array of "question objects"
+
+//this is an array of "question objects". Properties: question, answers, correct answer
 var myQuestions = [
-    {
-      question: "Who invented JavaScript?",
-      answers: {
-        a: "Douglas Crockford",
-        b: "Sheryl Sandberg",
-        c: "Brendan Eich"
-      },
-      correctAnswer: "c"
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
     },
-    {
-      question: "JavaScript is a ____-side programming language.",
-      answers: {
-        a: "Client",
-        b: "Server",
-        c: "Both"
-      },
-      correctAnswer: "c"
+    correctAnswer: "c"
+  },
+  {
+    question: "JavaScript is a ____-side programming language.",
+    answers: {
+      a: "Client",
+      b: "Server",
+      c: "Both"
     },
-    {
-      question: "What is the HTML tag under which one can write the JavaScript code?",
-      answers: {
-        a: "<style>",
-        b: "<script>",
-        c: "<javascript>",
+    correctAnswer: "c"
+  },
+  {
+    question: "What is the HTML tag under which one can write the JavaScript code?",
+    answers: {
+      a: "<style>",
+      b: "<script>",
+      c: "<javascript>",
+
+    },
+    correctAnswer: "b"
+  }
+
+]
+
+
+function jsQuiz() {
+  
+
+  }
+
+function startTimer() {
+    timeLeft = 10;
+
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+      // As long as the `timeLeft` is greater than 1
+      if (timeLeft > 1) {
+        // Set the `textContent` of `timerEl` to show the remaining seconds
+        timerEl.textContent = timeLeft + ' seconds remaining';
+        // Decrement `timeLeft` by 1
+        timeLeft--;
+      } else if (timeLeft === 1) {
+        // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+        timerEl.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+      } else {
+        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+        timerElement.textContent = '';
+        // Use `clearInterval()` to stop the timer
+        clearInterval(timeInterval);
         
-      },
-      correctAnswer: "b"
-    }
+        
+      }
+    }, 1000);
+  }
 
-
-function jsQuiz(){
-
-}
-
-function startTimer(){
-
-}
-
-startTest.addEventListener("click", jsQuiz);
+$startButton.addEventListener("click", jsQuiz);
 
 /*
 
@@ -65,6 +93,6 @@ no more questions or the timer reaches 0
 
 
 VARS
-array for questions 
+array for questions
 
 */
